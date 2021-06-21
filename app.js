245677306a5fec0ec530eec13820920e20be2355
cganzon <crganzon@gmail.com>
2021-06-20 20:01:14 -0700

    made the hero fully responsive

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 let burgerIcon = document.querySelector('.burger-icon');
 let closeIcon = document.querySelector('.close-icon');
 let navLinks = document.querySelector('.nav-links');
+let navBackground = document.querySelector('.nav-background');
 
 burgerIcon.addEventListener('click', () => {
     closeIcon.classList.toggle('hide');
     burgerIcon.classList.toggle('hide');
     navLinks.classList.toggle('hide');
+    navBackground.classList.toggle('hide');
 });
 
 closeIcon.addEventListener('click', () => {
     closeIcon.classList.toggle('hide');
     burgerIcon.classList.toggle('hide');
     navLinks.classList.toggle('hide');
+    navBackground.classList.toggle('hide');
 });
