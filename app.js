0367c52bed38106ced96650b24cace9e47125392
cganzon <crganzon@gmail.com>
2021-06-20 21:32:00 -0700

    mobile navbar has a fixed position when toggled to display

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 let burgerIcon = document.querySelector('.burger-icon');
 let closeIcon = document.querySelector('.close-icon');
+let navBar = document.querySelector('.navbar');
 let navLinks = document.querySelector('.nav-links');
 let navBackground = document.querySelector('.nav-background');
 
 burgerIcon.addEventListener('click', () => {
     closeIcon.classList.toggle('hide');
     burgerIcon.classList.toggle('hide');
+    navBar.classList.toggle('fixed');
     navLinks.classList.toggle('hide');
     navBackground.classList.toggle('hide');
+    navBackground.classList.toggle('fixed');
 });
 
 closeIcon.addEventListener('click', () => {
     closeIcon.classList.toggle('hide');
     burgerIcon.classList.toggle('hide');
+    navBar.classList.toggle('fixed');
     navLinks.classList.toggle('hide');
     navBackground.classList.toggle('hide');
+    navBackground.classList.toggle('fixed');
 });
